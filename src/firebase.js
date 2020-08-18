2a775606ba51ecdffc25faf5f4ec7a376b4e2af0
Piotr <grabowskipiotr1989@gmail.com>
2020-08-19 00:04:46 +0200

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBsv3ad3CUIb2UJ51-Y-hQXyfMLZjUjFFY",
+    authDomain: "whats-app-clone-40311.firebaseapp.com",
+    databaseURL: "https://whats-app-clone-40311.firebaseio.com",
+    projectId: "whats-app-clone-40311",
+    storageBucket: "whats-app-clone-40311.appspot.com",
+    messagingSenderId: "902480153643",
+    appId: "1:902480153643:web:121e826459ba19d3056e6e",
+    measurementId: "G-MZ8P2G359V"
+};
